80608d426bffb66df4f455f1faab6d3c8b746d6a
Sathila Thimalka <sathilathimalkad@gmail.com>
2024-12-05 19:45:31 +0530

    Email Authentication(Sign In)

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,121 +1,137 @@
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ToastAndroid } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { useNavigation, useRouter } from 'expo-router';
-import { Colors } from './../../../constants/Colors';
-import { Route } from 'expo-router/build/Route';
 import Ionicons from '@expo/vector-icons/Ionicons';
-
+import { Colors } from './../../../constants/Colors';
+import { auth } from './../../../configs/FirebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function SignIn() {
-  const navigation=useNavigation();
-  const router=useRouter();
-  
-useEffect(()=>{
-  navigation.setOptions({
-    headerShown:false
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  const onSignIn=()=>{
+
+    if (!email || !password ) {
+      ToastAndroid.show('Please Enter Email and Password', ToastAndroid.BOTTOM);
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
   })
-},[])
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage,error.code);
+    if(errorCode=='auth/invalid-credential')
+    {
+      ToastAndroid.show("invalid-credential",ToastAndroid.LONG)
+    }
+  });
+  }
+
 
   return (
-    <View style={{
-      padding:25,
-      paddingTop:40,
-      height:'100%',
-      backgroundColor:Colors.White,
-    }}>
-      <TouchableOpacity onPress={()=>router.back()}>
-      <Ionicons name="arrow-back" size={24} color="black" />
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="black" />
       </TouchableOpacity>
-      <Text style={{
-        fontFamily:'outfit Bold',
-        fontSize:30,
-        marginTop:30
-      }}>Sign In Your Account</Text>
+      <Text style={styles.header}>Sign In Your Account</Text>
+      <Text style={styles.subHeader}>Welcome Back</Text>
+      <Text style={styles.subHeader}>We Miss You!</Text>
 
-      <Text style={{
-        fontFamily:'outfit',
-        fontSize:30,
-        color:Colors.Gray,
-        marginTop:20
-      }}>Welcome Back</Text>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput 
+        style={styles.input} 
+        onChangeText={value=>setEmail(value)}
+        placeholder="Enter Email" />
+      </View>
 
-      <Text style={{
-        fontFamily:'outfit',
-        fontSize:30,
-        color:Colors.Gray,
-        marginTop:10
-      }}>We Miss You!</Text>
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Password</Text>
+        <TextInput style={styles.input} 
+        onChangeText={value=>setPassword(value)}
+        placeholder="Enter Password" secureTextEntry />
+      </View>
 
-    <View style={{
-      marginTop:50
-    }}>
-      <Text style={{
-        fontFamily:'outfit'
-      }}>Email</Text>
-      <TextInput
-      style={styles.input}
-      placeholder='Enter Email'/>
+      <TouchableOpacity onPress={onSignIn}
+      style={styles.primaryButton}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.secondaryButton} onPress={() => router.replace('auth/sign-up')}>
+        <Text style={styles.secondaryButtonText}>Create Account</Text>
+      </TouchableOpacity>
     </View>
-
-    <View style={{
-      marginTop:20
-    }}>
-      <Text style={{
-        fontFamily:'outfit',
-        
-      }}>Password
-      </Text>
-      <TextInput 
-      secureTextEntry={true}
-      style={styles.input}
-      placeholder='Enter Password'/>
-    </View>
-
-      {/*sign in button*/}
-    <View style={{
-      padding:20,
-      backgroundColor:Colors.Primary,
-      borderRadius:15,
-      marginTop:50,
-      
-    }}>
-      <Text style={{
-        textAlign:'center',
-        color:Colors.White,
-        fontFamily:'outfit'
-      }}>Sign In</Text>
-    </View>
-
-     {/*Create account button*/}
-     <TouchableOpacity
-     onPress={()=> router.replace("auth/sign-up")}
-
-     style={{
-      padding:20,
-      backgroundColor:Colors.White,
-      borderRadius:15,
-      marginTop:20,
-      borderWidth:1
-    }}>
-      <Text style={{
-        textAlign:'center',
-        color:Colors.Primary,
-        fontFamily:'outfit'
-      }}>Create Account</Text>
-    </TouchableOpacity>
-    </View>  
-    
-
-   
-    
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  input:{
-      padding:15,
-      borderWidth:1,
-      borderRadius:10,
-      borderColor:Colors.Gray
-  }
+  container: {
+    padding: 25,
+    paddingTop: 40,
+    height: '100%',
+    backgroundColor: Colors.White,
+  },
+  header: {
+    fontFamily: 'outfit Bold',
+    fontSize: 30,
+    marginTop: 30,
+  },
+  subHeader: {
+    fontFamily: 'outfit',
+    fontSize: 20,
+    color: Colors.Gray,
+    marginTop: 10,
+  },
+  inputGroup: {
+    marginTop: 20,
+  },
+  label: {
+    fontFamily: 'outfit',
+  },
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: Colors.Gray,
+  },
+  primaryButton: {
+    padding: 20,
+    backgroundColor: Colors.Primary,
+    borderRadius: 15,
+    marginTop: 50,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: Colors.White,
+    fontFamily: 'outfit',
+  },
+  secondaryButton: {
+    padding: 20,
+    backgroundColor: Colors.White,
+    borderRadius: 15,
+    marginTop: 20,
+    borderWidth: 1,
+  },
+  secondaryButtonText: {
+    textAlign: 'center',
+    color: Colors.Primary,
+    fontFamily: 'outfit',
+  },
 });
